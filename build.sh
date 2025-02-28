54b1c28edb786d8a5c7a6b74fda8fabee66cc4d7
amanmauryas <info.amanmauryas@gmail.com>
2025-02-28 14:23:09 +0530

    requirement.txt change

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 
 # Run migrations
 python manage.py migrate
